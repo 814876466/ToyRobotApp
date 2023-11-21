7a51bbf5a2a9706260ad725613cb4ae252dfbbce
Hannah.wang <hannah.wang@fdmgroup.com>
2023-11-21 16:27:26 +1100

    Initial commit of app.js and tests

---
+++ app.js
@@ -0,0 +1,169 @@
+const readlineSync = require("readline-sync");
+
+class Robot {
+  constructor() {
+    this.x = null;
+    this.y = null;
+    this.facing = null;
+    this.tableSize = 5;
+    this.directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+  }
+
+  place(x, y, facing) {
+    this.x = x;
+    this.y = y;
+    this.facing = facing;
+  }
+
+  executeActions(action) {
+    switch (action) {
+      case "MOVE":
+        this.move();
+        break;
+      case "LEFT":
+        this.left();
+        break;
+      case "RIGHT":
+        this.right();
+        break;
+      case "REPORT":
+        this.report();
+        break;
+      default:
+        console.warn("Invalid command! Please get instructions in README");
+    }
+  }
+
+  move() {
+    switch (this.facing) {
+      case "NORTH":
+        this.y = Math.min(this.y + 1, this.tableSize - 1);
+        break;
+      case "EAST":
+        this.x = Math.min(this.x + 1, this.tableSize - 1);
+        break;
+      case "SOUTH":
+        this.y = Math.max(this.y - 1, 0);
+        break;
+      case "WEST":
+        this.x = Math.max(this.x - 1, 0);
+        break;
+    }
+  }
+
+  left() {
+    const currentDirectionIndex = this.directions.indexOf(this.facing);
+    const afterLeft = this.leftIndex(currentDirectionIndex);
+    this.facing = this.directions[afterLeft];
+  }
+
+  leftIndex(leftIndex) {
+    leftIndex = leftIndex - 1;
+    if (leftIndex == -1) {
+      return 3;
+    }
+    return leftIndex;
+  }
+
+  right() {
+    const currentDirectionIndex = this.directions.indexOf(this.facing);
+    const afterRight = this.rightIndex(currentDirectionIndex);
+    this.facing = this.directions[afterRight];
+  }
+  rightIndex(rightIndex) {
+    rightIndex = rightIndex + 1;
+    if (rightIndex == 4) {
+      return 0;
+    }
+    return rightIndex;
+  }
+
+  report() {
+    console.log(`Report:${this.x},${this.y},${this.facing}`);
+  }
+}
+
+const robot = new Robot();
+
+function startSimulation() {
+  console.log("Welcome to Toy Robot Simulation!\n");
+
+  while (true) {
+    const command = readlineSync.question(
+      'Enter a command to start (or "EXIT" to end): '
+    );
+
+    if (command.toUpperCase() === "EXIT") {
+      console.log("Exiting the simulation.\n");
+      process.exit(1);
+    }
+
+    processCommand(command);
+  }
+}
+
+startSimulation();
+
+function processCommand(command) {
+  const parts = command.split(" ");
+  const action = parts[0].toUpperCase();
+
+  if (action == "PLACE") {
+    isPlaceValidate(parts);
+  } else if (parts.length !== 1) {
+    console.warn("Invalid command. Please enter one action.");
+  } else {
+    isBeenPlaced(action);
+  }
+}
+function isBeenPlaced(action) {
+  if (
+    !robot ||
+    !robot.place ||
+    robot.x === null ||
+    robot.y === null ||
+    robot.facing === null
+  ) {
+    console.warn("Warning: please PLACE the robot");
+    return;
+  } else {
+    robot.executeActions(action);
+  }
+}
+function isPlaceValidate(parts) {
+  console.log("valildate", parts);
+  if (parts.length !== 4) {
+    console.warn("please check the numbers of value of PLACE");
+    return;
+  } else {
+    const x = parseInt(isPositionRight(parts[1]));
+    const y = parseInt(isPositionRight(parts[2]));
+    const facing = isFacingRight(parts[3]);
+
+    robot.place(x, y, facing);
+  }
+}
+
+function isPositionRight(input) {
+  const intValue = parseFloat(input);
+
+  if (Number.isInteger(intValue) && intValue >= 0 && intValue <= 4) {
+    return intValue;
+  } else {
+    console.warn(`the robot position value ${input} is invalid`);
+    return;
+  }
+}
+
+function isFacingRight(input) {
+  const validDirection = ["NORTH", "EAST", "SOUTH", "WEST"];
+  const upperCaseInput = input.toUpperCase();
+  if (validDirection.includes(upperCaseInput)) {
+    return input;
+  } else {
+    console.warn(`The robot facing value ${input} is invalid`);
+    return;
+  }
+}
+
+module.exports = { robot, processCommand };
---
+++ app.test.js
@@ -0,0 +1,163 @@
+const { robot, processCommand } = require("./app");
+describe("test the robot executeActions method", () => {
+  test("action MOVE, it should move to NORTH correctly", () => {
+    robot.place(2, 2, "NORTH");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(2);
+    expect(robot.y).toBe(3);
+    expect(robot.facing).toBe("NORTH");
+  });
+  test("action MOVE to NORTH at the edge, it should not move", () => {
+    robot.place(0, 4, "NORTH");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(0);
+    expect(robot.y).toBe(4);
+    expect(robot.facing).toBe("NORTH");
+  });
+  test("action MOVE, it should move to EAST correctly", () => {
+    robot.place(0, 0, "EAST");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(0);
+    expect(robot.facing).toBe("EAST");
+  });
+  test("action MOVE to EAST at the edge, it should not move", () => {
+    robot.place(4, 0, "EAST");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(4);
+    expect(robot.y).toBe(0);
+    expect(robot.facing).toBe("EAST");
+  });
+  test("action MOVE, it should move to SOUTH correctly", () => {
+    robot.place(2, 2, "SOUTH");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(2);
+    expect(robot.y).toBe(1);
+    expect(robot.facing).toBe("SOUTH");
+  });
+  test("action MOVE to SOUTH at the edge, it should not move", () => {
+    robot.place(4, 0, "SOUTH");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(4);
+    expect(robot.y).toBe(0);
+    expect(robot.facing).toBe("SOUTH");
+  });
+  test("action MOVE, it should move to WEST correctly", () => {
+    robot.place(1, 1, "WEST");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(0);
+    expect(robot.y).toBe(1);
+    expect(robot.facing).toBe("WEST");
+  });
+  test("action MOVE to WEST at the edge, it should not move", () => {
+    robot.place(0, 0, "WEST");
+    robot.executeActions("MOVE");
+    expect(robot.x).toBe(0);
+    expect(robot.y).toBe(0);
+    expect(robot.facing).toBe("WEST");
+  });
+  test("action LEFT, It should turn LEFT correctly", () => {
+    robot.place(2, 2, "NORTH");
+    robot.executeActions("LEFT");
+    expect(robot.facing).toBe("WEST");
+  });
+
+  test("action LEFT, it should turn LEFT correctly", () => {
+    robot.place(2, 2, "SOUTH");
+    robot.executeActions("LEFT");
+    expect(robot.facing).toBe("EAST");
+  });
+
+  test("action RIGHT, It should turn RIGHT correctly", () => {
+    robot.place(2, 2, "WEST");
+    robot.executeActions("RIGHT");
+    expect(robot.facing).toBe("NORTH");
+  });
+
+  test("action RIGHT, it should turn RIGHT correctly", () => {
+    robot.place(2, 2, "NORTH");
+    robot.executeActions("RIGHT");
+    expect(robot.facing).toBe("EAST");
+  });
+});
+
+describe("test processCommand function", () => {
+  test("test the validate PLACE, ", () => {
+    processCommand("PLACE 1 0 NORTH");
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(0);
+    expect(robot.facing).toBe("NORTH");
+  });
+
+  test("test with valid POSITION, ACTION and REPORT , ", () => {
+    const consoleLogSpy = jest
+      .spyOn(console, "log")
+      .mockImplementation(() => {});
+    processCommand("PLACE 1 0 NORTH");
+    processCommand("MOVE");
+    processCommand("RIGHT");
+    processCommand("LEFT");
+    processCommand("move");
+    processCommand("REpoRT");
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(2);
+    expect(robot.facing).toBe("NORTH");
+    expect(consoleLogSpy).toHaveBeenCalledWith("Report:1,2,NORTH");
+
+    consoleLogSpy.mockRestore();
+  });
+});
+
+describe("test the valid command", () => {
+  beforeEach(() => {
+    consoleWarnSpy = jest.spyOn(console, "warn").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleWarnSpy.mockRestore();
+  });
+
+  test("test the sitution of no valild PLACE, it should return a warning ", () => {
+    robot.facing = null;
+
+    processCommand("move");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "Warning: please PLACE the robot"
+    );
+  });
+
+  test("test the PLACE validation, it should return a warning", () => {
+    processCommand("PLACE 1");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "please check the numbers of value of PLACE"
+    );
+  });
+  test("test the PLACE position validation, it should return a warning", () => {
+    processCommand("PLACE 1 -1 NORTH");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "the robot position value -1 is invalid"
+    );
+  });
+
+  test("test the PLACE facing validation, it should return a warning", () => {
+    processCommand("PLACE 1 0 abc");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "The robot facing value abc is invalid"
+    );
+  });
+
+  test("test the invalid ACTION, it should return a warning ", () => {
+    processCommand("PLACE 1 0 NORTH");
+    processCommand("ABC");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "Invalid command! Please get instructions in README"
+    );
+  });
+  test("test the invalid ACTION number,it should return a warning ", () => {
+    processCommand("PLACE 1 0 NORTH");
+    processCommand("MOVE LEFT");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "Invalid command. Please enter one action."
+    );
+  });
+});
